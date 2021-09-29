491675b6815d356707f547753a7069c30582082b
Eric Schmiel <eschmiel@gmail.com>
2021-09-29 14:32:33 -0400

    added buttons for navigating the different pages

--- src/main/orderTable.tsx
+++ src/main/orderTable.tsx
@@ -13,16 +13,17 @@ type OrderTableProps = {
 export default function OrderTable({ orders }: OrderTableProps) {
 
     const [pageNumber, setPage] = useState(1);
+    const maxPageEntries = 5;
 
     function generateOrderPage() {
         let orderPage: JSX.Element[] = [];
         let countLimit = 0;
-        let countStart = (pageNumber - 1) * 10; //This should provide the array index of the first order for the current page. 
+        let countStart = (pageNumber - 1) * maxPageEntries; //This should provide the array index of the first order for the current page. 
                                                 //i.e. countStart is 0 on page 1, 10 on page 2, 20 on page 3
 
-        if (orders.length < pageNumber * 10) 
-            countLimit = pageNumber * 10; //10 entries per page
-        else countLimit = orders.length - 1;
+        if (orders.length > pageNumber * maxPageEntries)
+            countLimit = pageNumber * maxPageEntries; //10 entries per page
+        else countLimit = orders.length;
 
 
         for (countStart; countStart < countLimit; countStart++) {
@@ -32,10 +33,31 @@ export default function OrderTable({ orders }: OrderTableProps) {
         return orderPage;
     }
 
+    function incrementPageNumber() {
+        setPage(pageNumber + 1);
+    }
+
+    function decrementPageNumber() {
+        setPage(pageNumber - 1);
+    }
+
     return (
         <div id='orderTable'>
             <OrderTableLabels />
             {generateOrderPage()}
+            <div id='pageControls'>
+                {pageNumber === 1 ?
+                    <div className='pageButton inactive'>{'<'}</div>
+                    : <div className='pageButton clickable' onClick={decrementPageNumber}>{'<'}</div>
+                }
+
+                <div>{pageNumber}</div>
+
+                {pageNumber * maxPageEntries < orders.length ?
+                    <div className='pageButton clickable' onClick={incrementPageNumber}>{'>'}</div>
+                    : <div className='pageButton inactive'>{'>'}</div>
+                }
+            </div>
         </div>
     );
 }
